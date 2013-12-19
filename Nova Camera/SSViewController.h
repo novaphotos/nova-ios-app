d0600508bdb7750b0381998740a55ce2d60cc8be
Mike Matz <mike@pixor.net>
2013-12-20 08:38:53 +1100

    Initial Commit

---
+++ Nova Camera/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  Nova Camera
+//
+//  Created by Mike Matz on 12/20/13.
+//  Copyright (c) 2013 Sneaky Squid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSViewController : UIViewController
+
+@end
---
+++ Nova Camera/SSViewController.m
@@ -0,0 +1,29 @@
+//
+//  SSViewController.m
+//  Nova Camera
+//
+//  Created by Mike Matz on 12/20/13.
+//  Copyright (c) 2013 Sneaky Squid. All rights reserved.
+//
+
+#import "SSViewController.h"
+
+@interface SSViewController ()
+
+@end
+
+@implementation SSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
